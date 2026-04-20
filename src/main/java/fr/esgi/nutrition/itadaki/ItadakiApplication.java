10de5402c568b1cc065e15cc1258c4ab7b469c26
CharlesCar0105 <charles.cartier@etu.u-paris.fr>
2026-04-20 10:37:58 +0200

    Initialize Spring Boot application with Maven setup and configuration files

---
+++ src/main/java/fr/esgi/nutrition/itadaki/ItadakiApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.nutrition.itadaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItadakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItadakiApplication.class, args);
+	}
+
+}
